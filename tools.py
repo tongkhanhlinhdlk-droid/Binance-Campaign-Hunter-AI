a6f40c89d780fcebbf432c27c660119edd24c082
LinhInsights <tongkhanhlinh.dlk@gmail.com>
2026-03-14 14:19:16 +0700

    Add campaign tools

---
+++ tools.py
@@ -0,0 +1,11 @@
+def get_campaigns():
+    return [
+        {
+            "name": "Binance Launchpool",
+            "reward": "Token rewards"
+        },
+        {
+            "name": "Trading Competition",
+            "reward": "USDT prizes"
+        }
+    ]
